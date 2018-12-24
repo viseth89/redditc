9fb38da066bfa89152dfd6d39ee0c28932b8adb7
viseth89 <viseth89@gmail.com>
2018-12-23 21:49:50 -0600

    #67 finishing posts app

--- accounts/views.py
+++ accounts/views.py
@@ -19,18 +19,13 @@ def signup(request):
     else:
         return render(request, 'accounts/signup.html')
 
-def login(request):
+def loginview(request):
     if request.method == 'POST':
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                User.objects.get(username=request.POST['username'])
-                return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
-            except User.DoesNotExist:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-                login(request, user)
-                return render(request, 'accounts/signup.html')
-            # password2=request.POST['password2']
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return render(request, 'accounts/login.html', {'error':'Login Successful!'})
         else:
-            return render(request, 'accounts/signup.html', {'error':'Passwords do not match'})
+            return render(request, 'accounts/login.html', {'error':'The Username and password did not match'})
     else:
         return render(request, 'accounts/login.html')
---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    url(r'^create/', views.create, name='create'),
+]
